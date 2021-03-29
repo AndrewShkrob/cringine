13d99019181333574abb90fae737c26c3917b3d0
Whiskarek <andrew.shkrob.social@yandex.by>
2021-03-29 12:25:13 +0300

    moved demo project to study folder

--- study/create_window/main.cpp
+++ study/create_window/main.cpp
@@ -11,17 +11,11 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 }
 
 int main() {
-    //Инициализация GLFW
     glfwInit();
-    //Настройка GLFW
-    //Задается минимальная требуемая версия OpenGL.
-    //Мажорная
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    //Минорная
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //Установка профайла для которого создается контекст
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //Выключение возможности изменения размера окна
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
