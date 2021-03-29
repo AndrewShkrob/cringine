239a4c24c1f6e3cf74461f25475c1b842a263c7a
Whiskarek <andrew.shkrob.social@yandex.by>
2021-03-29 23:46:00 +0300

    updated first study project

--- study/create_window/main.cpp
+++ study/create_window/main.cpp
@@ -1,30 +1,11 @@
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
+
+#include <cringine/window/window.hpp>
+
 #include <iostream>
 
-
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode) {
-    // Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true,
-    // и приложение после этого закроется
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
 int main() {
-    glfwInit();
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
-    if (window == nullptr) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
+    cringine::window window(800, 600, "LearnOpenGL");
 
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
@@ -32,22 +13,12 @@ int main() {
         return -1;
     }
 
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, window.width(), window.height());
 
-    glViewport(0, 0, width, height);
-
-    glfwSetKeyCallback(window, key_callback);
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
+    window.launch([]() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+    });
 
-        glfwSwapBuffers(window);
-    }
-
-    glfwTerminate();
     return 0;
 }
