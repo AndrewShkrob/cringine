f1726e354fe2c6f797ddb84ca801e26a91310b60
Whiskarek <andrew.shkrob.social@yandex.by>
2021-04-04 18:24:40 +0300

    added some methods to shader program class

--- libs/graphics/cringine/graphics/shader_program.cpp
+++ libs/graphics/cringine/graphics/shader_program.cpp
@@ -2,17 +2,34 @@
 
 using namespace cringine;
 
-cringine::shader_program::shader_program(GLuint program)
+shader_program::shader_program(GLuint program)
     : m_program(program)
 {
 }
 
-void cringine::shader_program::use() const
+void shader_program::use() const
 {
     glUseProgram(m_program);
 }
 
-GLuint cringine::shader_program::program() const
+GLuint shader_program::program() const
 {
     return m_program;
+}
+
+void shader_program::set_uniform_3f(const std::string& uniform_name, GLfloat v0, GLfloat v1, GLfloat v2) const
+{
+    GLint uniform_location = get_uniform_location(uniform_name);
+    glUniform3f(uniform_location, v0, v1, v2);
+}
+
+void shader_program::set_uniform_matrix4fv(const std::string& uniform_name, GLsizei count, GLboolean transpose, const GLfloat* value) const
+{
+    GLint uniform_location = get_uniform_location(uniform_name);
+    glUniformMatrix4fv(uniform_location, count, transpose, value);
+}
+
+GLint shader_program::get_uniform_location(const std::string& uniform_name) const
+{
+    return glGetUniformLocation(m_program, uniform_name.c_str());
 }
