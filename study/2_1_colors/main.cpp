838391cd65cce38393b11a579bfb32b8e5cdf392
Whiskarek <andrew.shkrob.social@yandex.by>
2021-04-04 17:53:04 +0300

    removed comment

--- study/2_1_colors/main.cpp
+++ study/2_1_colors/main.cpp
@@ -95,7 +95,6 @@ int main()
         last_frame = currentFrame;
         move_camera(camera, delta_time, keys);
 
-//        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shaderProgram.use();
