0d92b38d31e020ab403e687acfd4e0114cc76ad2
VelesInteractive <valentine.ptashnik@gmail.com>
2021-04-14 23:25:29 +0300

    changed parameter names

--- libs/event_system/cringine/event_system/events/window_position.hpp
+++ libs/event_system/cringine/event_system/events/window_position.hpp
@@ -5,7 +5,7 @@ namespace cringine::event_system::events
     class window_position_event
     {
     public:
-        virtual void window_position(int width, int height) = 0;
+        virtual void window_position(int x, int y) = 0;
     };
 
     using window_position_event_ptr = window_position_event*;
