c859548ba884115d0e3a33109d0cdef3c2eba848
VelesInteractive <valentine.ptashnik@gmail.com>
2021-04-15 00:49:26 +0300

    added new functionality to input_manager

---
+++ libs/event_system/cringine/event_system/input/input_manager.cpp
@@ -0,0 +1,113 @@
+#include "input_manager.hpp"
+
+using namespace cringine::event_system::input;
+
+void input_manager::bind_to_event_system(event_system_sptr event_system)
+{
+    event_system->register_cursor_focus_observer(this);
+    event_system->register_cursor_position_observer(this);
+    event_system->register_keyboard_input_observer(this);
+    event_system->register_mouse_button_input_observer(this);
+    event_system->register_scroll_input_observer(this);
+}
+
+void input_manager::update_states()
+{
+    update_key_states();
+    update_button_states();
+    update_scroll_delta();
+}
+
+bool input_manager::is_cursor_focused() const
+{
+    return m_cursor_focus;
+}
+
+std::pair<double, double> input_manager::cursor_position() const
+{
+    return std::make_pair(m_cursor_x, m_cursor_y);
+}
+
+double input_manager::cursor_position_x() const
+{
+    return m_cursor_x;
+}
+
+double input_manager::cursor_position_y() const
+{
+    return m_cursor_y;
+}
+
+std::pair<double, double> input_manager::scroll_delta() const
+{
+    return std::make_pair(m_scroll_delta_x, m_scroll_delta_y);
+}
+
+double input_manager::scroll_delta_x() const
+{
+    return m_scroll_delta_x;
+}
+
+double input_manager::scroll_delta_y() const
+{
+    return m_scroll_delta_y;
+}
+
+bool input_manager::is_key_pressed(keys::key_t key) const
+{
+    return m_key_current_states[key] && !m_key_previous_states[key];
+}
+
+bool input_manager::is_key_released(keys::key_t key) const
+{
+    return !m_key_current_states[key] && m_key_previous_states[key];
+}
+
+bool input_manager::is_key_down(keys::key_t key) const
+{
+    return m_key_current_states[key];
+}
+
+bool input_manager::is_key_up(keys::key_t key) const
+{
+    return !m_key_current_states[key];
+}
+
+bool input_manager::is_mouse_button_pressed(keys::key_t key) const
+{
+    return m_button_current_states[key] && !m_button_previous_states[key];
+}
+
+bool input_manager::is_mouse_button_released(keys::key_t key) const
+{
+    return !m_button_current_states[key] && m_button_previous_states[key];
+}
+
+bool input_manager::is_mouse_button_down(keys::key_t key) const
+{
+    return m_button_current_states[key];
+}
+
+bool input_manager::is_mouse_button_up(keys::key_t key) const
+{
+    return !m_button_current_states[key];
+}
+
+void input_manager::update_key_states()
+{
+    for (auto i = 0u; i < s_MAX_KEY_AMOUNT; ++i) {
+      m_key_previous_states[i] = m_key_current_states[i];
+    }
+}
+
+void input_manager::update_button_states()
+{
+    for (auto i = 0u; i < s_MAX_BUTTON_AMOUNT; ++i) {
+      m_button_previous_states[i] = m_button_current_states[i];
+    }
+}
+
+void input_manager::update_scroll_delta()
+{
+    m_scroll_delta_x = m_scroll_delta_y = 0;
+}
--- libs/event_system/cringine/event_system/input/input_manager.hpp
+++ libs/event_system/cringine/event_system/input/input_manager.hpp
@@ -1,14 +1,56 @@
 #pragma once
 
 #include <cringine/event_system/input/keys.hpp>
+#include <cringine/event_system/event_system.hpp>
+
+#include <array>
 
 namespace cringine::event_system::input
 {
-    class input_manager
+    class input_manager : virtual public events::cursor_focus_event, virtual public events::cursor_position_event,
+                          virtual public events::keyboard_input_event, virtual public events::mouse_button_input_event,
+                          virtual public events::scroll_input_event
     {
     public:
+        input_manager() = default;
+        virtual ~input_manager() = default;
+
+        void bind_to_event_system(event_system_sptr event_system);
+
+        void update_states();
+
+        bool is_cursor_focused() const;
+        std::pair<double, double> cursor_position() const;
+        double cursor_position_x() const;
+        double cursor_position_y() const;
+        std::pair<double, double> scroll_delta() const;
+        double scroll_delta_x() const;
+        double scroll_delta_y() const;
+        bool is_key_pressed(keys::key_t key) const;
+        bool is_key_released(keys::key_t key) const;
+        bool is_key_down(keys::key_t key) const;
+        bool is_key_up(keys::key_t key) const;
+        bool is_mouse_button_pressed(keys::key_t key) const;
+        bool is_mouse_button_released(keys::key_t key) const;
+        bool is_mouse_button_down(keys::key_t key) const;
+        bool is_mouse_button_up(keys::key_t key) const;
 
     private:
-//        std::array<bool, keys::KEY_AMOUNT> m_key_states{false};
+        void update_key_states();
+        void update_button_states();
+        void update_scroll_delta();
+
+    protected:
+        bool m_cursor_focus = true;
+        double m_cursor_x = 0, m_cursor_y = 0;
+        double m_scroll_delta_x = 0, m_scroll_delta_y = 0;
+
+        static constexpr auto s_MAX_KEY_AMOUNT = 512u;
+        std::array<bool, s_MAX_KEY_AMOUNT> m_key_current_states{false}, m_key_previous_states{false};
+
+        static constexpr auto s_MAX_BUTTON_AMOUNT = 8u;
+        std::array<bool, s_MAX_BUTTON_AMOUNT> m_button_current_states{false}, m_button_previous_states{false};
     };
+
+    using input_manager_sptr = std::shared_ptr<input_manager>;
 } // namespace cringine::event_system::input
