b4c2b2a598716c68d428458c857e8b3c0ea70d89
Whiskarek <andrew.shkrob.social@yandex.by>
2021-04-21 16:56:07 +0300

    writing example

--- examples/test/main.cpp
+++ examples/test/main.cpp
@@ -1,7 +1,44 @@
 #include <cringine/core/engine.hpp>
+#include <cringine/models/model_loader.hpp>
+#include <cringine/renderer/types/rendered_mesh.hpp>
+#include <cringine/renderer/mesh_renderer.hpp>
+#include <cringine/shaders/shader_program_builder.hpp>
+#include <cringine/shaders/shader_data_binder.hpp>
+#include <cringine/types/camera.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
 #include <cmath>
+
+class model
+{
+public:
+    explicit model(std::string model_path)
+        : m_model(cringine::models::load_model(std::move(model_path)))
+    {
+        m_mesh_renderer.reserve(m_model.meshes.size());
+        m_rendered_mesh.reserve(m_model.meshes.size());
+        for (cringine::types::object::mesh_data_sptr mesh : m_model.meshes) {
+            m_rendered_mesh.emplace_back(mesh);
+            m_mesh_renderer.emplace_back(m_rendered_mesh.back());
+        }
+    }
+
+    void render(cringine::shaders::shader& shader)
+    {
+        for (auto& renderer : m_mesh_renderer) {
+            renderer.render(shader);
+        }
+    }
+
+private:
+    std::vector<cringine::renderer::mesh_renderer> m_mesh_renderer;
+    std::vector<cringine::renderer::types::rendered_mesh> m_rendered_mesh;
+    cringine::models::types::mesh_model m_model;
+};
 
 class a : cringine::event_system::events::window_close_event
 {
@@ -9,13 +46,37 @@ public:
     void launch()
     {
         cringine::engine engine({800, 600, "Hello"});
+
+        auto m = model(std::string(RESOURCES) + "models/backpack/backpack.obj");
+        cringine::shaders::shader s = cringine::shaders::shader_program_builder()
+                                          .add_fragment_shader(std::string(RESOURCES) + "shaders/3_3_models/model.fragment")
+                                          .add_vertex_shader(std::string(RESOURCES) + "shaders/3_3_models/model.vertex")
+                                          .build();
+        cringine::shaders::shader_data_binder binder(s);
+
+        cringine::types::camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
         engine.event_system()->register_window_close_callback(this);
-        engine.start([&engine]() {
-            auto r = (std::sin(engine.time()) + 1) / 2;
-            auto g = (std::cos(engine.time()) + 1) / 2;
-            auto b = r;
-            glClearColor(1.0f * r, 1.0f * g, 1.0f * b, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+
+
+        engine.start([&]() {
+            glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            s.use();
+            glm::mat4 projection = glm::perspective(camera.zoom(), (float) 800 / (float) 600, 0.1f, 100.0f);
+            glm::mat4 view = camera.view_matrix();
+            binder.set_uniform_matrix4fv("projection", 1, false, glm::value_ptr(projection));
+            binder.set_uniform_matrix4fv("view", 1, false, glm::value_ptr(view));
+
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+            model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+
+            binder.set_uniform_matrix4fv("model", 1, false, glm::value_ptr(model));
+
+            m.render(s);
 
             std::cout << "FPS: " << engine.fps() << " Delta time: " << engine.delta_time() << std::endl;
         });
